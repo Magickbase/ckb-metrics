5255d8fcdbbf8348926ff215997acd00d550cbb0
Keith <keithwhisper@gmail.com>
2024-06-22 02:21:05 +0900

    feat: support get method for holder allocation

--- next.config.js
+++ next.config.js
@@ -12,6 +12,17 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+        ],
+      },
+    ]
+  },
 }
 
 export default config
--- src/app/api/holder_allocation/route.ts
+++ src/app/api/holder_allocation/route.ts
@@ -1,4 +1,28 @@
+import { type NextRequest } from 'next/server'
+import { type Script } from '@ckb-lumos/lumos'
 import { api } from '@/trpc/server'
+import { computeScriptHash } from '@ckb-lumos/lumos/utils'
+
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = req.nextUrl
+  const codeHash = searchParams.get('code_hash')
+  const hashType = searchParams.get('hash_type')
+  const args = searchParams.get('args')
+  if (!codeHash || !hashType || !args) {
+    return new Response('codeHash, hashType, args are required', { status: 400 })
+  }
+  const typeScript = { codeHash, hashType, args } as Script
+  const typeHash = computeScriptHash(typeScript)
+
+  const allocations = await api.ckb.holder.holderAllocation([{ typeScript, typeHash }])
+  const allocation = allocations.get(typeHash)
+
+  if (!allocation) {
+    return new Response('holder allocation not found', { status: 404 })
+  }
+
+  return Response.json(allocation)
+}
 
 export const POST = async (req: Request) => {
   const { scripts } = await req.json()
@@ -8,11 +32,5 @@ export const POST = async (req: Request) => {
     })
   }
   const holders = await api.ckb.holder.holderAllocation(scripts)
-  return new Response(JSON.stringify(Object.fromEntries(holders)), {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  })
+  return Response.json(Object.fromEntries(holders))
 }
